48cf9e58c5d72f45ca006ed7a5e8e117fedff675
uliasuprun <uliasuprun88@gmail.com>
2022-05-25 13:39:15 +0300

    19

--- script.js
+++ script.js
@@ -328,4 +328,35 @@
 
 // ***** ZADACHA 19 *****
 
+// Функция checkPassword(password) получает пароль пользователя в параметр password, проверяет его на совпадение с паролем администратора в переменной ADMIN_PASSWORD и возвращает сообщение о результате сравнения, хранящееся в переменной message.
+
+// Если значение параметра password равно null, значит пользователь отменил операцию и в message записывается строка "Canceled by user!".
+// Если значение параметра password совпадает со значением ADMIN_PASSWORD, в переменную message присваивается строка "Welcome!".
+// Если ни одно из предыдущих условий не выполнилось, в переменную message записывается строка "Access denied, wrong password!".
+// Объявлена функция checkPassword(password)
+// Вызов checkPassword("mangohackzor") возвращает "Access denied, wrong password!"
+// Вызов checkPassword(null) возвращает "Canceled by user!"
+// Вызов checkPassword("polyhax") возвращает "Access denied, wrong password!"
+// Вызов checkPassword("jqueryismyjam") возвращает "Welcome!"
+
+function checkPassword(password) {
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
+
+if (password === null) { // Change this line
+    message =  'Canceled by user!';
+} else if (password === ADMIN_PASSWORD) { // Change this line
+    message = 'Welcome!';
+} else {
+    message = 'Access denied, wrong password!';
+}
+
+return message;
+}
+
+console.log(checkPassword("mangohackzor"));
+console.log(checkPassword(null));
+console.log(checkPassword("polyhax"));
+console.log(checkPassword("jqueryismyjam"));
+
 
