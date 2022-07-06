23b29f4b96d3d592c13663b704744197e538bc53
uliasuprun <uliasuprun88@gmail.com>
2022-07-06 14:30:58 +0300

    module 3

---
+++ module3.js
@@ -0,0 +1,48 @@
+// ***** 1
+
+const playlist = {
+    name: 'Мой плейлист',
+    rating: 5,
+    tracks: ['трек-1', 'трек-2', 'трек-3'],
+    trackCount: 3,
+};
+console.log(playlist);
+console.log(playlist.name);
+console.log(playlist.rating);
+console.log(playlist.tracks);
+console.log(playlist.trackCount);
+
+const propertyName = 'tracks';
+console.log(playlist.rating);
+console.log(playlist['rating']);
+
+
+// const fn = function (myObject) {
+//     console.log(myObject)
+// };
+// fn({a:1, b:2,});
+
+
+// const sfrgf = function() {
+//     return {};
+// };
+// console.log(sfrgf);
+
+// ***** 2
+
+// ***** 3
+
+// ***** 4
+
+// ***** 5
+
+// ***** 6
+
+// ***** 7
+
+// ***** 8
+
+// ***** 9
+
+// ***** 10
+
